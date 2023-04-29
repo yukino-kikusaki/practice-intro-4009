5c1ea8c776e9dd38b94a679ef097a0493b8b928b
Azumabashi <hiro_kataoka@hkataoka.local>
2023-04-29 10:53:17 +0900

    apply modification of past sections

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+'use strict';
+
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const helmet = require('helmet');
 const session = require('express-session');
 const passport = require('passport');
@@ -11,11 +13,11 @@ const GitHubStrategy = require('passport-github2').Strategy;
 const GITHUB_CLIENT_ID = 'f756acb8748f85e2014b';
 const GITHUB_CLIENT_SECRET = '0fc57f6660bd5da78873eeacda8c131859b64f30';
 
-passport.serializeUser(function (user, done) {
+passport.serializeUser((user, done) => {
   done(null, user);
 });
 
-passport.deserializeUser(function (obj, done) {
+passport.deserializeUser((obj, done) => {
   done(null, obj);
 });
 
@@ -24,19 +26,19 @@ passport.use(new GitHubStrategy({
   clientSecret: GITHUB_CLIENT_SECRET,
   callbackURL: 'http://localhost:8000/auth/github/callback'
 },
-  function (accessToken, refreshToken, profile, done) {
-    process.nextTick(function () {
+  (accessToken, refreshToken, profile, done) => {
+    process.nextTick(() => {
       return done(null, profile);
     });
   }
 ));
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 const photosRouter = require('./routes/photos');
-var serverStatusRouter = require('./routes/server-status');
+const serverStatus = require('./routes/server-status');
 
-var app = express();
+const app = express();
 app.use(helmet());
 
 // view engine setup
@@ -54,44 +56,39 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', indexRouter);
-app.use('/users', ensureAuthenticated, usersRouter);
+app.use('/users', usersRouter);
 app.use('/photos', photosRouter);
-app.use('/server-status', serverStatusRouter);
+app.use('/server-status', serverStatus);
 
 app.get('/auth/github',
   passport.authenticate('github', { scope: ['user:email'] }),
-  function (req, res) {
-  });
+  (req, res) => {
+});
 
 app.get('/auth/github/callback',
   passport.authenticate('github', { failureRedirect: '/login' }),
-  function (req, res) {
+  (req, res) => {
     res.redirect('/');
-  });
-
-app.get('/login', function (req, res) {
-  res.render('login', { user: req.user });
 });
 
-app.get('/logout', function (req, res, next) {
-  req.logout(function (err) {
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/logout', (req, res, next) => {
+  req.logout((err) => {
     if (err) return next(err);
     res.redirect('/');
   });
 });
 
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login');
-}
-
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
